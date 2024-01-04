4dacf41b714eee98f4a46541850495447aedee6c
contentguy <gogumag3@naver.com>
2024-01-04 21:55:47 +0900

    init

---
+++ src/part1/chapter1.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, test } from "@jest/globals";
+import { Dollar } from "./chapter1";
+
+// Chapter 1
+describe("testMultiplication", () => {
+  test("testMultiplication", () => {
+    const five = new Dollar(5);
+    five.times(2);
+    expect(five.amount).toBe(10);
+  });
+});
