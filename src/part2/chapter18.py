f500689a511bc7cd7344f26e266e316ec9d97885
contentguy <gogumag3@naver.com>
2024-01-09 00:51:01 +0900

    feat: 2024-01-09

--- src/part2/chapter18.py
+++ src/part2/chapter18.py
@@ -4,14 +4,29 @@
 # 테스트 케이스 작성 -> 테스트 메서드 실행
 
 # 1. 테스트 케이스가 호출되면 true, 아니면 false
-class WasRun:
+class TestCase:
+  def __init__(self, name):
+    self.name = name
+  def run(self):
+    method = getattr(self, self.name)
+    method()
+
+class WasRun(TestCase):
   def __init__(self, name):
     self.wasRun = None
+    TestCase.__init__(self, name)
   def testMethod(self):
     self.wasRun = 1
 
-test = WasRun("testMethod")
-print (test.wasRun)
-test.testMethod()
-print (test.wasRun)
+class TestCaseTest (TestCase) :
+  def testRunning(self):
+    test = WasRun("testMethod")
+    assert(not test.wasRun)
+    test.run()
+    assert(test.wasRun)
 
+TestCaseTest("testRunning").run()
+
+# 리팩토링 패턴
+# 1. 두 부분으로 나눠서 차별화된 기능을 테스트한 후, 병합하든지 그대로 둔다.
+# 2. 특별한 하나의 사례를 가져와 구현한 뒤, 상수를 변수로 변경하여 일반화한다.
---
+++ src/part2/chapter19.py
@@ -0,0 +1,46 @@
+# Chapter 19 테이블 차리기
+
+# 빌웨이크(Bill Wake)의 3A (p.169)
+# 준비(arrage) : 객체를 생성한다.
+# 행동(act) : 어떤 자극을 준다.
+# 확인(assert) : 결과를 검사한다.
+
+# 테스트를 위해 새로운 객체를 얼마나 자주 생성해야 하는가?
+# 성능과 격리의 상충된 제약
+
+# *** 테스트 커플링을 만들지 말 것 ***
+
+class TestCase:
+  def __init__(self, name):
+    self.name = name
+  def setUp(self):
+    pass
+  def run(self):
+    self.setUp()
+    method = getattr(self, self.name)
+    method()
+
+class WasRun(TestCase):
+  def __init__(self, name):
+    self.wasRun = None
+    TestCase.__init__(self, name)
+  def testMethod(self):
+    self.wasRun = 1
+  def setUp(self):
+    self.wasRun = None
+    self.wasSetUp = 1
+
+class TestCaseTest (TestCase) :
+  def setUp(self) :
+    self.test = WasRun("testMethod")
+  def testRunning(self):
+    test = WasRun("testMethod")
+    test.run()
+    assert(test.wasRun)
+  def testSetUp(self):
+    self.test.run()
+    assert(self.test.wasSetUp)
+
+TestCaseTest("testRunning").run()
+
+# 테스트의 실행을 확인 한 후, 한 번에 하나의 메서드만 수정해서 테스트가 통과되도록 노력
