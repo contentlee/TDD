aaf24c2203aafd6ecb756a1d71c12d36b3652977
YUNTAEK LEE <85660408+contentlee@users.noreply.github.com>
2024-04-18 01:15:32 +0900

    Update chapter12.ts

--- docs/part1/chapter12.ts
+++ docs/part1/chapter12.ts
@@ -43,9 +43,9 @@ export class Money implements Expression {
   // 다중 통화 연산을 위해 고민의 시간이 필요하다.
   // 다중 통화 사용에 대한 내용을 시스템의 내부로 숨기고 싶다는 생각이 강할 것이다.
   // 방법 중 하나는 참조 통화로 모든 값을 전환하는 것이다.
-  // 단점은 여러 환율을 쓰기가 수비지 않다는 점이다.
+  // 단점은 여러 환율을 쓰기가 쉽지 않다는 점이다.
   // 이를 해결하기 위해 우리는 객체를 사용할 것이다.
-  // 객체와 외부 프로토콜이 같으면서내부 구현은 다른 imposter를 만들어 해결한다.
+  // 객체와 외부 프로토콜이 같으면서 내부 구현은 다른 imposter를 만들어 해결한다.
   plus(addend: Money): Expression {
     return new Money(this.amount + addend.amount, this.currency);
   }
